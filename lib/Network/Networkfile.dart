17b0481ebb4188f5d868b0cf9ce35ea87000b00e
CyberCadence <86359099+CyberCadence@users.noreply.github.com>
2022-06-01 17:08:06 +0530

    network done

--- lib/Network/Networkfile.dart
+++ lib/Network/Networkfile.dart
@@ -1,20 +1,18 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:weatherapi/Model/weathermodel.dart';
 
 class Repository {
-  Future getdata({required String location}) async {
+  Future<Weather> getdata({required String location}) async {
     String url =
         'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c1b66d13f980552b4cdd1123f5c20c72#';
 
     final result = await http.get(Uri.parse(url));
 
-    if (result.statusCode == 200) {
-       final decodeddata=  jsonDecode(result.body);
-
-
-
-    }
-  } 
+    final decodeddata = jsonDecode(result.body);
+      return Weather.fromJson(decodeddata);
+       
+   
+  }
 }
- 
