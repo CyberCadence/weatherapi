1723baf2d5f371e4097018b571bd2882de30535a
CyberCadence <86359099+CyberCadence@users.noreply.github.com>
2022-06-01 18:28:05 +0530

    data fetched,

--- lib/Network/Networkfile.dart
+++ lib/Network/Networkfile.dart
@@ -4,15 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:weatherapi/Model/weathermodel.dart';
 
 class Repository {
-  Future<Weather> getdata({required String location}) async {
+  Future<Weather?> getdata({required String location}) async {
     String url =
         'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c1b66d13f980552b4cdd1123f5c20c72#';
 
     final result = await http.get(Uri.parse(url));
-
+    if (result.statusCode != 200) {
+      return null;
+    }
     final decodeddata = jsonDecode(result.body);
-      return Weather.fromJson(decodeddata);
-       
-   
+    print(Weather.fromJson(decodeddata).cityname);
+    return Weather.fromJson(decodeddata);
   }
 }
--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:weatherapi/Model/weathermodel.dart';
+import 'package:weatherapi/Network/Networkfile.dart';
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Repository repository = Repository();
+
+  Weather? weather;
+  Future<void> getdata() async {
+    weather = await repository.getdata(location: 'thrissur');
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    Repository repository = Repository();
+    return MaterialApp(
+        home: Scaffold(
+            body: FutureBuilder(
+                future: getdata(),
+                builder: ((context, snapshot) {
+                  return Center(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+
+
+                      Text(weather!.temp.toString())
+                   ,Text(weather!.cityname.toString()), ],),
+                  );
+                }))));
+  }
+}
