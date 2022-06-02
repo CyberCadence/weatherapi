0c7e80e3047316a24b07f76822932fdcb9c9b677
CyberCadence <86359099+CyberCadence@users.noreply.github.com>
2022-06-02 19:46:22 +0530

    all done..ui pending

--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
- 
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weatherapi/Model/weathermodel.dart';
 import 'package:weatherapi/Network/Networkfile.dart';
- 
+
 import 'package:weatherapi/bloc/weather_bloc.dart';
 
 TextEditingController textEditingController = TextEditingController();
@@ -17,6 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String? location;
   @override
   Widget build(BuildContext context) {
     {
@@ -25,15 +26,20 @@ class _MyAppState extends State<MyApp> {
           home: Scaffold(body: BlocBuilder<WeatherBloc, WeatherState>(
         builder: (context, state) {
           if (state is WeatherInitialstate) {
-            context.read<WeatherBloc>().add(
-                loadWeatherDataEvent(location: textEditingController.text));
-
-            return const Center(
-              child: CircularProgressIndicator(color: Colors.red,),
+            return Center(
+              child: TextField(
+                controller: textEditingController,
+                onSubmitted: (location) {
+                  context.read<WeatherBloc>().add(loadWeatherDataEvent(
+                      location: textEditingController.text));
+                },
+              ),
             );
           } else if (state is WeatherLoadingstate) {
             return const Center(
-              child: CircularProgressIndicator(color: Colors.yellow,),
+              child: CircularProgressIndicator(
+                color: Colors.yellow,
+              ),
             );
           } else if (state is Weatherloadedstate) {
             return buildnew(state.weathermodel);
@@ -51,45 +57,19 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// Future<Position> determinePosition() async {
-//   bool serviceEnabled;
-//   LocationPermission permission;
-
-//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//   if (!serviceEnabled) {
-//     return Future.error('Location services are disabled.');
-//   }
-
-//   permission = await Geolocator.checkPermission();
-//   if (permission == LocationPermission.denied) {
-//     permission = await Geolocator.requestPermission();
-//     if (permission == LocationPermission.denied) {
-//       return Future.error('Location permissions are denied');
-//     }
-//   }
-
-//   if (permission == LocationPermission.deniedForever) {
-//     return Future.error(
-//         'Location permissions are permanently denied, we cannot request permissions.');
-//   }
-
-//   return await Geolocator.getCurrentPosition();
-// }
-
 Widget buildnew(Weather weather) {
   return Column(
     children: [
       TextField(
           controller: textEditingController,
-          onChanged: (value) {
-        
-          },
+          onChanged: (value) {},
           decoration: InputDecoration(
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(30),
             ),
           )),
-      Text(weather.cityname.toString())
+      Text(weather.cityname.toString()),
+      Text(weather.temp.toString())
     ],
   );
 }
